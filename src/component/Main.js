be5b01729edefd95f01cac8b3a1040d43988998e
Jay <jjjjuhyuneee@naver.com>
2022-09-11 15:22:38 +0900

    0911

--- src/component/Main.js
+++ src/component/Main.js
@@ -2,15 +2,13 @@ const Main = () => {
     return (
         <>
             <section className="main_visual">
-                <div className="container">
-                    <h2><span>THE</span><br />
-                        <strong>MISSION OF</strong>
-                    </h2>
-                    <div className="main_object">
-                        <img src={process.env.PUBLIC_URL + "/assets/img/mv_obj.png"} alt="" />
-                    </div>
+                <div className="slogan">
+                    <p>THE</p>
+                    <strong>MISSION OF</strong>
                 </div>
-
+                <div className="main_object">
+                    <img src={process.env.PUBLIC_URL + "/assets/img/mv_obj.png"} alt="" />
+                </div>
             </section>
             <section className="story_of_apollo">
                 <div className="container">
