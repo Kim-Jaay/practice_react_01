daea950ffeee7c22263f9be0294e33d87c58a21d
Jay <jjjjuhyuneee@naver.com>
2022-09-13 20:17:53 +0900

    0913

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,6 +1,21 @@
 const Footer = () => {
     return (
         <>
+            <div className="container">
+                <h1><a href=""><img src={process.env.PUBLIC_URL + "/assets/img/logo.svg"} alt="" /></a></h1>
+                <ul className="footerLeft">
+                    <li>National Aeronautics and Space Administration</li>
+                    <li>NASA Official: Brian Dunbar</li>
+                </ul>
+                <ul className="footerMenu">
+                    <li><a href="">No Fear Act</a></li>
+                    <li><a href="">FOIA</a></li>
+                    <li><a href="">Privacy</a></li>
+                    <li><a href="">Office of Inspector General</a></li>
+                    <li><a href="">Agency Financial Reports</a></li>
+                    <li><a href="">Contact NASA</a></li>
+                </ul>
+            </div>
 
 
 
